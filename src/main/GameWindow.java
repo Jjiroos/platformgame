29b118fd53f1523143f01a835dc819459fe25bfa
Sc0vil <cloclo2412@gmail.com>
2023-09-11 17:11:35 +0200

    first commit of the project : creation of the screen components

---
+++ src/main/GameWindow.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+    JFrame gameScreen;
+    public GameWindow(GamePanel gamePanel){
+        gameScreen  = new JFrame();
+        gameScreen.setSize(400, 400);
+        gameScreen.add(gamePanel);
+        gameScreen.setVisible(true);
+        gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
